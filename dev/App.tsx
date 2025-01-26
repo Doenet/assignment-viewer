44b98d53b568bf6be460a74b4af8e3b12a320a4c
Duane Nykamp <nykamp@umn.edu>
2025-01-25 20:40:00 -0600

    add settings to test app

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,13 +1,201 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { AssignmentViewer } from "../src/assignment-builder";
 import { AssignmentSource } from "../src/types";
 import assignmentSource from "./testAssignment.json";
 
 import initialAssignmentState from "./testInitialState.json";
-import { AssignmentState } from "../src/Viewer/assignmentState";
+import { AssignmentState, ItemId } from "../src/Viewer/assignmentState";
 
 function App() {
+    const defaultTestSettings: {
+        requestedVariantIndex: number;
+        showCorrectness: boolean;
+        readOnly: boolean;
+        showFeedback: boolean;
+        showHints: boolean;
+        questionLevelAttempts: boolean;
+        assignmentLevelAttempts: boolean;
+        shuffle: boolean;
+    } = {
+        requestedVariantIndex: 1,
+        showCorrectness: true,
+        readOnly: false,
+        showFeedback: true,
+        showHints: true,
+        questionLevelAttempts: true,
+        assignmentLevelAttempts: true,
+        shuffle: false,
+    };
+
+    const [controlsVisible, setControlsVisible] = useState(false);
+    const [testSettings, setTestSettings] = useState(defaultTestSettings);
+    const [updateNumber, setUpdateNumber] = useState(0);
+
+    const {
+        requestedVariantIndex,
+        showCorrectness,
+        readOnly,
+        showFeedback,
+        showHints,
+        questionLevelAttempts,
+        assignmentLevelAttempts,
+        shuffle,
+    } = testSettings;
+
+    let controls = null;
+    let buttonText = "show";
+    if (controlsVisible) {
+        buttonText = "hide";
+        controls = (
+            <div style={{ padding: "8px" }}>
+                <p>
+                    The assignment is displayed is loaded from the file:{" "}
+                    <code>dev/testAssignment.json</code>.
+                </p>
+
+                <div>
+                    <button
+                        onClick={() => {
+                            setTestSettings(defaultTestSettings);
+                            setUpdateNumber((was) => was + 1);
+                        }}
+                    >
+                        Reset
+                    </button>
+                </div>
+                <hr />
+                <div>
+                    <label>
+                        {" "}
+                        <input
+                            type="checkbox"
+                            checked={showCorrectness}
+                            onChange={() => {
+                                setTestSettings((was) => {
+                                    const newObj = { ...was };
+                                    newObj.showCorrectness =
+                                        !was.showCorrectness;
+                                    return newObj;
+                                });
+                                setUpdateNumber((was) => was + 1);
+                            }}
+                        />
+                        Show Correctness
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        {" "}
+                        <input
+                            type="checkbox"
+                            checked={readOnly}
+                            onChange={() => {
+                                setTestSettings((was) => {
+                                    const newObj = { ...was };
+                                    newObj.readOnly = !was.readOnly;
+                                    return newObj;
+                                });
+                                setUpdateNumber((was) => was + 1);
+                            }}
+                        />
+                        Read Only
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        {" "}
+                        <input
+                            type="checkbox"
+                            checked={showFeedback}
+                            onChange={() => {
+                                setTestSettings((was) => {
+                                    const newObj = { ...was };
+                                    newObj.showFeedback = !was.showFeedback;
+                                    return newObj;
+                                });
+                                setUpdateNumber((was) => was + 1);
+                            }}
+                        />
+                        Show Feedback
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        {" "}
+                        <input
+                            type="checkbox"
+                            checked={showHints}
+                            onChange={() => {
+                                setTestSettings((was) => {
+                                    const newObj = { ...was };
+                                    newObj.showHints = !was.showHints;
+                                    return newObj;
+                                });
+                                setUpdateNumber((was) => was + 1);
+                            }}
+                        />
+                        Show Hints
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        {" "}
+                        <input
+                            type="checkbox"
+                            checked={questionLevelAttempts}
+                            onChange={() => {
+                                setTestSettings((was) => {
+                                    const newObj = { ...was };
+                                    newObj.questionLevelAttempts =
+                                        !was.questionLevelAttempts;
+                                    return newObj;
+                                });
+                                setUpdateNumber((was) => was + 1);
+                            }}
+                        />
+                        Question level attempts
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        {" "}
+                        <input
+                            type="checkbox"
+                            checked={assignmentLevelAttempts}
+                            onChange={() => {
+                                setTestSettings((was) => {
+                                    const newObj = { ...was };
+                                    newObj.assignmentLevelAttempts =
+                                        !was.assignmentLevelAttempts;
+                                    return newObj;
+                                });
+                            }}
+                        />
+                        Assignment level attempts
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        {" "}
+                        <input
+                            type="checkbox"
+                            checked={shuffle}
+                            onChange={() => {
+                                setTestSettings((was) => {
+                                    const newObj = { ...was };
+                                    newObj.shuffle = !was.shuffle;
+                                    return newObj;
+                                });
+                            }}
+                        />
+                        Shuffle question order
+                    </label>
+                </div>
+            </div>
+        );
+    }
+
     const assignmentId = "apple";
 
     const [assignmentState, setAssignmentState] = useState<AssignmentState>(
@@ -45,19 +233,94 @@ function App() {
         };
     }, []);
 
+    const [itemIdToOrigItemIdx, itemWeights] = useMemo(() => {
+        const idToIdx: Record<ItemId, number> = {};
+        let weights = [];
+        let totalWeight = 0;
+        for (const [idx, item] of assignmentSource.items.entries()) {
+            idToIdx[item.id] = idx;
+            if (item.type === "question") {
+                const w = item.weight ?? 1;
+                weights.push(w);
+                totalWeight += w;
+            } else {
+                weights.push(0);
+            }
+        }
+        weights = weights.map((w) => w / totalWeight);
+
+        return [idToIdx, weights];
+    }, []);
+
     console.log({ score, assignmentState });
+
+    const lastestAssignmentAttempt =
+        assignmentState.attempts[assignmentState.assignmentAttemptNumber - 1];
+
     return (
         <>
-            <h1>Test assignment builder</h1>
+            <div
+                style={{
+                    backgroundColor: "lightGray",
+                    width: "800px",
+                    marginLeft: "20px",
+                    padding: "10px",
+                }}
+            >
+                <h3>
+                    <div style={{ display: "flex" }}>
+                        Test assignment viewer
+                        <button
+                            onClick={() => {
+                                setControlsVisible((was) => !was);
+                            }}
+                            style={{ marginLeft: "12px" }}
+                        >
+                            {buttonText + " controls"}
+                        </button>
+                    </div>
+                </h3>
+                {controls}
+
+                <div>Assignment credit: {assignmentState.creditAchieved}</div>
+                <div>
+                    Credit by item, latest attempt:
+                    <ol>
+                        {assignmentState.attempts[
+                            assignmentState.assignmentAttemptNumber - 1
+                        ]?.items
+                            .filter(
+                                (item) =>
+                                    assignmentSource.items[
+                                        itemIdToOrigItemIdx[item.itemId]
+                                    ].type === "question",
+                            )
+                            .map((item) => (
+                                <li key={item.itemId}>{item.creditAchieved}</li>
+                            ))}
+                    </ol>
+                </div>
+                <div>
+                    Assignment attempt number:{" "}
+                    {assignmentState.assignmentAttemptNumber}
+                </div>
+            </div>
+
             <AssignmentViewer
                 source={assignmentSource as AssignmentSource}
                 flags={{
+                    showCorrectness,
+                    readOnly,
+                    showFeedback,
+                    showHints,
                     allowSaveEvents: true,
                     allowSaveState: true,
                     allowLoadState: true,
                 }}
                 shuffle={true}
                 assignmentId={assignmentId}
+                questionLevelAttempts={questionLevelAttempts}
+                assignmentLevelAttempts={assignmentLevelAttempts}
             />
         </>
     );
